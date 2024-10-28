d7133c8bc4e7d086335d39ec50f86bfbcbb2a0ed
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-10-28 11:12:50 +0000

    .

--- Utils/Event.py
+++ Utils/Event.py
@@ -3,5 +3,3 @@ from enum import Enum, auto
 class Event(Enum):
     UPDATE_GAME = auto()
     KEY_PRESSED = auto()
-    # Snake
-    EAT = auto()
--- Utils/Subject.py
+++ Utils/Subject.py
@@ -1,26 +1,27 @@
 import pygame
 from pygame import Surface
-from pygame.sprite import Group
 from pygame.time import Clock
 import logging
 import json
-from typing import Generator
+from abc import ABC, abstractmethod
 
 from Utils import Event, Observer, Events
 
-class Subject(Observer):
+class Subject(Observer, ABC):
 
     def __init__(self):
         Observer.__init__(self)
         pygame.init()
         self.fps: int = 60
-        self.display_size: tuple[int, int] = (800, 600)
         self.running: bool = True
-        self.sprites: Group = Group()
+        self.paused: bool = False
         self.display: Surface = None
         self.clock: Clock = Clock()
 
-        self.frame = 0
+        self.frame: int = 0
+        self.logs: list[dict] = []
+        self.replay: list[tuple[int, list]] = [] # frame, keys
+
         logging.basicConfig(
             filename="game.log",
             format='%(message)s',
@@ -29,64 +30,79 @@ class Subject(Observer):
         )
 
 
+    @abstractmethod
+    def render(self) -> None:
+        pass
+
+
+    @property
+    def replaying(self) -> bool:
+        return bool(self.replay)
+
+
+    def set_fps(self, fps: int) -> None:
+        self.fps = fps
+
+
+    def set_display_size(self, width: int, height: int) -> None:
+        self.display = pygame.display.set_mode((width, height))
+
+
+    def set_replay(self, logs: list[dict]) -> None:
+        self.frame = 0
+        self.replay = [
+            (log["frame"], log["keys"])
+            for log in logs
+            if log["event"] == Event.KEY_PRESSED.name and log.get("keys")
+        ]
+
+
     def process_input(self) -> None:
         pressed_keys = []
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
-            elif event.type == pygame.KEYDOWN:
+            elif (
+                event.type == pygame.KEYDOWN
+                or event.type == pygame.K_ESCAPE
+                or not self.replaying
+                or self.paused
+            ):
                 Events.add(Event.KEY_PRESSED, key=event.key)
                 pressed_keys.append(event.key)
-        logging.info(json.dumps({
-            "frame": self.frame,
-            "event": "KEY_PRESSED",
-            "keys": pressed_keys
-        }))
+
+        if self.replaying:
+            frame, keys = self.replay[0]
+            if frame == self.frame:
+                for key in keys:
+                    Events.add(Event.KEY_PRESSED, key=key)
+                self.replay.pop(0)
+            pressed_keys = keys
+
+        if pressed_keys:
+            self.log(Event.KEY_PRESSED, keys=pressed_keys)
         self.frame += 1
 
 
+    def log(self, event: Event, **kwargs) -> None:
+        msg = json.dumps({
+            "frame": self.frame,
+            "event": event.name,
+            **kwargs
+        })
+        self.logs.append(msg)
+        logging.info(msg)
+
+
     def update_game(self) -> None:
+        Events.add(Event.UPDATE_GAME)
         for event, kwargs in Events.get():
             Events.notify(event, **kwargs)
 
 
-    def render(self) -> None:
-        self.display.fill("white")
-        self.sprites.update()
-        self.sprites.draw(self.display)
-        pygame.display.flip()
-        self.clock.tick(self.fps)
-
-
-    def replay(self, file) -> Generator:
-        with open(file, "r") as f:
-            lines = f.readlines()
-        for i, line in enumerate(lines):
-            data = json.loads(line)
-            if data["event"] == "KEY_PRESSED":
-                for key in data["keys"]:
-                    Events.add(Event.KEY_PRESSED, key=key)
-                logging.info(json.dumps({
-                    "frame": self.frame,
-                    "event": "KEY_PRESSED",
-                    "keys": data["keys"]
-                }))
-                self.frame += 1
-                if i == len(lines) - 1:
-                    self.running = False
-                yield
-            
-
-    def run(self, file=None) -> None:
-        self.display = pygame.display.set_mode(self.display_size)
-        if file:
-            replay = self.replay(file)
+    def run(self) -> None:
         while self.running:
-            if file:
-                next(replay)
-            else:
-                self.process_input()
-            Events.add(Event.UPDATE_GAME)
+            self.process_input()
             self.update_game()
             self.render()
         pygame.quit()
