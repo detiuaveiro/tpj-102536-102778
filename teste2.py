1e60e14b613d415c6d65f78b10b94a2d07e45c04
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-12-03 12:02:20 +0000

    scroll test

--- teste2.py
+++ teste2.py
@@ -1,7 +1,7 @@
 import pygame
-from utils import Subject, Event
-from Game.Menu import Menu
 
+from utils import Subject, Event
+# from entities import Menu
 
 class Teste(Subject):
 
@@ -9,36 +9,39 @@ class Teste(Subject):
         super().__init__()
         self.fps = 90
         self.set_display_size(1000, 800)
-        self.menu = Menu(self)
+        # self.menu = Menu(self)
         self.register_events(
             Event.KEY_DOWN,
-            Event.NEW_LEVEL
         )
-        self.paused = True
-        self.level = 1
-
-
-    def on_new_frame(self):
-        self.keys = []
+        self.paused = False
+        self.surface = pygame.Surface((900, 700))
+        self.surface_rect = self.surface.get_rect()
+        self.surface_rect.center = (500, 350)
 
 
     def on_key_down(self, key):
-        print(key)
+        if key == pygame.K_LEFT:
+            self.surface_rect.move_ip(-50, 0)
+        elif key == pygame.K_RIGHT:
+            self.surface_rect.move_ip(50, 0)
+        elif key == pygame.K_UP:
+            self.surface_rect.move_ip(0, 50)
+        elif key == pygame.K_DOWN:
+            self.surface_rect.move_ip(0, -50)
+        print(self.surface_rect.center)
 
 
-    def on_new_level(self, level):
-        self.level = level
+    def draw_box(self, surface):
+        pygame.draw.rect(surface, "black", (100, 100, 200, 200))
 
 
     def draw(self):
         self.display.fill("white")
-        text = pygame.font.SysFont(None, 50).render("Hello World", True, "black")
-        text_rect = text.get_rect(center=(500, 400))
-        self.display.blit(text, text_rect)
-        level_text = pygame.font.SysFont(None, 50).render(f"Level: {self.level}", True, "black")
-        level_text_rect = level_text.get_rect(center=(500, 500))
-        self.display.blit(level_text, level_text_rect)
-        self.menu.draw(self.display)
+        self.surface.fill("blue")
+        self.draw_box(self.surface)
+        self.display.blit(self.surface, self.surface_rect)
+        # self.menu.draw(self.display)
+
         
 
     
