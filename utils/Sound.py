63386e98515c84038931a41bbc2850e322304f95
Leonardo Almeida <leonardoalmeida7777@gmail.com>
2024-12-11 14:42:28 +0000

    docstring

--- utils/Sound.py
+++ utils/Sound.py
@@ -4,11 +4,31 @@ from time import time
 from game.consts import SOUND_COOLDOWN, SOUNDS
 
 class Sound:
+    """
+    Singleton class that manages the sounds in the game.
+
+    Static Attributes
+    -----------------
+    _sounds (dict[str, int]): 
+        Dictionary of sounds and their last played time.
+    """
 
     _sounds: dict[str, int] = {}
 
+    def __new__(cls) -> None:
+        raise Exception("This class is a singleton.")
+    
+
     @staticmethod
     def play_sound(name: str) -> None:
+        """
+        Play a sound.
+
+        Parameters
+        ----------
+        name (str):
+            Name of the sound.
+        """
         path = SOUNDS[name]
         last = Sound._sounds.get(path, 0)
         now = time()
